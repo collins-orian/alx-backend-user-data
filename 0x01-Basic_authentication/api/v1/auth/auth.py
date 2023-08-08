eb31def879334ddcc9d7ced3b895946a063b5bc5
collins-orian <inaboyacollins@icloud.com>
2023-08-08 21:46:52 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,10 +8,16 @@ from typing import List, TypeVar
 class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication.
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """gets the authorization header from the request"""
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """gets the current user from the request"""
         return None
