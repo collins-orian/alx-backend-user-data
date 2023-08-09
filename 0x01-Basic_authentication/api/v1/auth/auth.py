4ba56f3601d9270a4ca91c96f9c49087467cd49b
collins-orian <inaboyacollins@icloud.com>
2023-08-09 21:57:19 +0100

    task 4 done

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Module for Authentication"""
 from flask import request as req
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -10,7 +11,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if a path requires authentication."""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """gets the authorization header from the request"""
