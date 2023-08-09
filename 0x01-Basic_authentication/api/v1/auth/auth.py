a58afc1dc6053e864244ca9cda57a7b56a520cb0
collins-orian <inaboyacollins@icloud.com>
2023-08-09 20:52:44 +0100

    retrying task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,18 +7,13 @@ from typing import List, TypeVar
 class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if a path requires authentication.
-        """
+        """Checks if a path requires authentication."""
         return False
 
     def authorization_header(self, request=None) -> str:
         """gets the authorization header from the request"""
-        if request is not None:
-            return req.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """gets the current user from the request"""
-        if request is not None:
-            return req.headers.get('Authorization', None)
         return None
