f867fc34f10173dd33d84968e28c826431512474
collins-orian <inaboyacollins@icloud.com>
2023-08-10 02:54:48 +0100

    task 7 done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 """Module for Basic Authentication"""
 from .auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """This class handles Basic Authentication"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        # checks if the authorization header is a string
+        if type(authorization_header) == str:
+            # uses regular expression to extract the base64 encoded string
+            pattern = r'Basic (?P<token>.+)'
+            #  checks if the authorization header contains a valid token
+            part_match = re.fullmatch(pattern, authorization_header.strip())
+            # if the part_match returns true, the return the token
+            if part_match is not None:
+                return part_match.group('token')
+        return None
