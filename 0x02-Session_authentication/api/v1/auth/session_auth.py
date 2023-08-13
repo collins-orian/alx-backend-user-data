5d3ed4a12ae3ab5e73c2c24dc2384a17536c44ea
collins-orian <inaboyacollins@icloud.com>
2023-08-13 22:45:37 +0100

    task 2 done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 """Module for Session Authentication"""
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """This class handles Session Authentication"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if type(user_id) == str:
+            sess_id = str(uuid4())
+            self.user_id_by_session_id[sess_id] = user_id
+            return sess_id
